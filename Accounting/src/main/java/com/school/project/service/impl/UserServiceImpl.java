c6d3b348a66cd8c5fd1504d2190e346ac52c8b9a
My Name KVA <immobilien.net.ru@gmail.com>
2019-05-23 21:27:57 +0200

    add flyway

---
+++ Accounting/src/main/java/com/school/project/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.school.project.service.impl;
+
+import com.school.project.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
