5e55666a7fb8bbb37ac416878700fa0f73ce9ee1
venividiviciregina <venividiviciregina@gmail.com>
2019-05-25 17:26:40 +0200

    Add flyway

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into student
+values(10001,'Robert', 'W1234567');
+
+insert into student
+values(10002,'Alex', 'W1234568');
+
+INSERT INTO `I_LANGUAGE`(`ID`, `DESCRIPTION`, `LANGUAGE_NATIVE_NAME`, `IS_ACTIVE`) VALUES (1,'en','English',TRUE);
+INSERT INTO `I_LANGUAGE`(`ID`, `DESCRIPTION`, `LANGUAGE_NATIVE_NAME`, `IS_ACTIVE`) VALUES (2,'ru','Русский',TRUE);
