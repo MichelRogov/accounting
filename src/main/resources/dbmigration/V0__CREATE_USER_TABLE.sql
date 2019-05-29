a24ff0a6ea5e6ff06e74ee0f46f5d376baa17c95
venividiviciregina <venividiviciregina@gmail.com>
2019-05-29 04:40:54 +0200

    add flyway

---
+++ src/main/resources/dbmigration/V0__CREATE_USER_TABLE.sql
@@ -0,0 +1,10 @@
+create table USER
+(
+ID           bigint  not null_auto_increment,
+NAME         varchar(50),
+EMAIL        varchar(50) not null,
+PASSWORD     varchar(50) not null,
+CREATED_DATE datetime    not null,
+UPDATED_DATE datetime    not null,
+primary key(ID)
+);
