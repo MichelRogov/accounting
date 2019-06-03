b861b89f40c6d8c0dd28ddfd85bcf2b48d0a2d7d
My Name KVA <immobilien.net.ru@gmail.com>
2019-06-03 17:13:01 +0200

    Add flyway and New User

--- src/main/resources/dateBase/migration/V1_00__INITIALIZE.sql
+++ src/main/resources/dateBase/migration/V1_00__INITIALIZE.sql
@@ -12,4 +12,22 @@ create table USER
 );
 
 alter table USER
+    add constraint USER_ID unique (ID);
+alter table USER
+    add constraint USER_FIRST_NAME unique (FIRST_NAME);
+alter table USER
+    add constraint USER_LAST_NAME unique (LAST_NAME);
+alter table USER
+    add constraint USER_BIRTH_DATE unique (BIRTH_DATE);
+alter table USER
     add constraint USER_EMAIL unique (EMAIL);
+alter table USER
+    add constraint USER_PHONE_NUMBER unique (PHONE_NUMBER);
+alter table USER
+    add constraint USER_CREATED_DATE unique (CREATED_DATE);
+alter table USER
+    add constraint USER_UPDATED_DATE unique (UPDATED_DATE);
+
+
+
+
