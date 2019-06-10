1929bc4d5c25dfd866ca62917a51fc2d3002b03a
maximorlenco <maxim.orlenco@gmail.com>
2019-06-10 21:52:19 +0200

    Fix naming and spelling

--- src/main/resources/db/migration/V2_0__INITIALIZE.sql
+++ src/main/resources/db/migration/V2_0__INITIALIZE.sql
@@ -1,4 +1,4 @@
-create table GROUPS
+create table STUDENT_GROUP
 (
     ID         bigint   not null auto_increment,
     START_DATE datetime not null,
@@ -6,30 +6,30 @@ create table GROUPS
     primary key (ID)
 );
 
-create table GROUP_USERS
+create table STUDENT_GROUP_USER
 (
-    USER_ID   bigint,
-    GROUPS_ID bigint not null
+    USER_ID          bigint,
+    STUDENT_GROUP_ID bigint not null
 );
 
 
 create table LESSON
 (
-    ID          bigint       not null auto_increment,
-    LESSON_DATE datetime     not null,
-    THEMA       varchar(255) not null,
-    GROUP_ID    bigint       not null,
-    SUBJECT_ID  bigint       not null,
-    TEACHER_ID  bigint       not null,
+    ID               bigint       not null auto_increment,
+    LESSON_DATE      datetime     not null,
+    THEMA            varchar(255) not null,
+    STUDENT_GROUP_ID bigint       not null,
+    SUBJECT_ID       bigint       not null,
+    TEACHER_ID       bigint       not null,
     primary key (ID)
 );
 
 create table MODULE
 (
-    ID bigint       not null auto_increment,
-    HOURS     integer      not null,
-    NAME      varchar(255) not null,
-    PRICE     double precision,
+    ID    bigint       not null auto_increment,
+    HOURS integer      not null,
+    NAME  varchar(255) not null,
+    PRICE double precision,
     primary key (ID)
 );
 
@@ -40,10 +40,10 @@ create table SUBJECT
     primary key (ID)
 );
 
-create table MODULE_SUBJECTS
+create table MODULE_SUBJECT
 (
-    MODULE_ID bigint not null,
-    SUBJECT_ID      bigint not null
+    MODULE_ID  bigint not null,
+    SUBJECT_ID bigint not null
 );
 create table ATTENDANCE
 (
@@ -56,22 +56,22 @@ create table ATTENDANCE
 
 
 alter table ATTENDANCE
-    add constraint ATTENDANCE_LESSON_ID foreign key (LESSON_ID) references LESSON (ID);
+    add constraint ATTENDANCE_LESSON_ID_LESSON_ID foreign key (LESSON_ID) references LESSON (ID);
 alter table ATTENDANCE
-    add constraint ATENDANCS_USER_ID foreign key (USER_ID) references USER (ID);
-alter table GROUP_USERS
-    add constraint GROUP_USERS_USER_ID_USER_ID foreign key (USER_ID) references USER (ID);
-alter table GROUP_USERS
-    add constraint GROUP_USERS_GROUP_ID_GROUPS_ID foreign key (GROUPS_ID) references GROUPS (ID);
-alter table GROUPS
-    add constraint GROUPS_MODULE_ID_MODULE_ID foreign key (MODULE_ID) references MODULE (ID);
+    add constraint ATENDANCE_USER_ID_USER_ID foreign key (USER_ID) references USER (ID);
+alter table STUDENT_GROUP_USER
+    add constraint STUDENT_GROUP_USER_USER_ID_USER_ID foreign key (USER_ID) references USER (ID);
+alter table STUDENT_GROUP_USER
+    add constraint GROUP_USER_GROUP_ID_GROUP_ID foreign key (STUDENT_GROUP_ID) references STUDENT_GROUP (ID);
+alter table STUDENT_GROUP
+    add constraint STUDENT_GROUP_MODULE_ID_MODULE_ID foreign key (MODULE_ID) references MODULE (ID);
 alter table LESSON
-    add constraint LESSON_GROUP_ID_GROUP_ID foreign key (GROUP_ID) references GROUPS (ID);
+    add constraint LESSON_GROUP_ID_GROUP_ID foreign key (STUDENT_GROUP_ID) references STUDENT_GROUP (ID);
 alter table LESSON
     add constraint LESSON_SUBJECT_ID_SUBJECT_ID foreign key (SUBJECT_ID) references SUBJECT (ID);
 alter table LESSON
     add constraint LESSON_TEACHER_ID_USER_ID foreign key (TEACHER_ID) references USER (ID);
-alter table MODULE_SUBJECTS
-    add constraint MODUKE_SUBJECTS_SUBJECT_ID_SUBJECT_ID foreign key (SUBJECT_ID) references SUBJECT (ID) ;
-alter table MODULE_SUBJECTS
-    add constraint MODULE_SUBJECTS_MODULE_ID_MODULE_ID foreign key (MODULE_ID) references MODULE (ID);
+alter table MODULE_SUBJECT
+    add constraint MODULE_SUBJECT_SUBJECT_ID_SUBJECT_ID foreign key (SUBJECT_ID) references SUBJECT (ID);
+alter table MODULE_SUBJECT
+    add constraint MODULE_SUBJECT_MODULE_ID_MODULE_ID foreign key (MODULE_ID) references MODULE (ID);
--- src/main/resources/db/migration/V2_1__MODULE_SUBJECT.sql
+++ src/main/resources/db/migration/V2_1__MODULE_SUBJECT.sql
@@ -1,20 +1,19 @@
 INSERT INTO MODULE (ID, HOURS, NAME, PRICE)
 VALUES (1, 40, 'BASIC_JAVA', 400.00),
        (2, 35, 'JAVA', 700.00),
-       (3, 89, 'WEBINARS', 500.50),
-       (4, 41, 'FRONTEDN', 645.25);
+       (3, 89, 'DEVOPS', 500.50),
+       (4, 41, 'FRONTEND', 645.25);
 
 INSERT INTO SUBJECT (ID, NAME)
-VALUES
-(1,'BACKEND'),
-(2,'FRONTEND'),
-(3,'QA');
+VALUES (1, 'BACKEND'),
+       (2, 'FRONTEND'),
+       (3, 'QA');
 
-INSERT INTO MODULE_SUBJECTS (MODULE_ID, SUBJECT_ID)
-VALUES (1,3),
-       (2,1),
-       (1,2),
-       (4,2),
-       (3,2),
-       (4,1);
+INSERT INTO MODULE_SUBJECT (MODULE_ID, SUBJECT_ID)
+VALUES (1, 3),
+       (2, 1),
+       (1, 2),
+       (4, 2),
+       (3, 2),
+       (4, 1);
 
