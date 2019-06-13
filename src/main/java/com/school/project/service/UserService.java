66062ef571a6e0f329edb262903125828ba094b9
Your Name <you@example.com>
2019-06-13 21:12:48 +0200

    Stoped using userRepository in userController

--- src/main/java/com/school/project/service/UserService.java
+++ src/main/java/com/school/project/service/UserService.java
@@ -3,14 +3,18 @@ package com.school.project.service;
 import com.school.project.dto.UserDto;
 import com.school.project.model.entities.User;
 
+import java.util.List;
+
 public interface UserService {
 
-    User create(User User);
+    User create(UserDto userDto);
 
     void update(UserDto user, Long id);
 
     void delete(Long id);
 
-    User getUserById(Long id);
+    UserDto getUserById(Long id);
+
+    List<UserDto> getAllUsers();
 
 }
--- src/main/java/com/school/project/service/impl/UserServiceImpl.java
+++ src/main/java/com/school/project/service/impl/UserServiceImpl.java
@@ -8,14 +8,19 @@ import com.school.project.repository.UserAccountRepository;
 import com.school.project.repository.UserRepository;
 import com.school.project.service.UserAccountService;
 import com.school.project.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    ModelMapper modelMapper;
     @Autowired
     UserRepository userRepository;
     @Autowired
@@ -25,15 +30,19 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
-    public User create(User user) {
-        userRepository.save(user);
-        userAccountService.createUserAccount(user, UserAccountType.STUDENT);
-        return user;
+    public User create(UserDto userDto) {
+        User userForCreate = convertUserDtoToUser(userDto);
+        userRepository.save(userForCreate);
+        userAccountService.createUserAccount(userForCreate, UserAccountType.STUDENT);
+        return userForCreate;
     }
 
     @Override
     public void update(UserDto user, Long id) {
-        User userById = getUserById(id);
+        User userById;
+        Optional<User> byId = userRepository.findById(id);
+        if (!byId.isPresent()) throw new UserNotFoundException("User with id :" + id + "is not found");
+        else userById = byId.get();
         userById.setFirstName(user.getFirstName());
         userById.setLastName(user.getLastName());
         userById.setBirthDate(user.getBirthDate());
@@ -49,9 +58,23 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getUserById(Long id) {
+    public UserDto getUserById(Long id) {
         Optional<User> byId = userRepository.findById(id);
         if (!byId.isPresent()) throw new UserNotFoundException("User with id :" + id + "is not found");
-        return byId.get();
+        return convertUserToUserDto(byId.get());
+    }
+
+    @Override
+    public List<UserDto> getAllUsers(){
+        return userRepository.findAll().stream()
+                .map(s -> convertUserToUserDto(s))
+                .collect(Collectors.toList());
+    }
+    private User convertUserDtoToUser(UserDto userDto) {
+        return modelMapper.map(userDto, User.class);
+    }
+
+    private UserDto convertUserToUserDto(User user) {
+        return modelMapper.map(user, UserDto.class);
     }
 }
