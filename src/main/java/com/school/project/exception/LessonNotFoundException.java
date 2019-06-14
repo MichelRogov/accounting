566319879edcfc92a8bcd5aeb5f7b231ff4ed66d
maximorlenco <maxim.orlenco@gmail.com>
2019-06-14 10:31:28 +0200

    Modifie LessonService Impl , adding Exception

---
+++ src/main/java/com/school/project/exception/LessonNotFoundException.java
@@ -0,0 +1,7 @@
+package com.school.project.exception;
+
+public class LessonNotFoundException  extends RuntimeException{
+
+    public LessonNotFoundException(String msg) { super(msg);}
+
+}
--- src/main/java/com/school/project/service/impl/LessonServiceimpl.java
+++ src/main/java/com/school/project/service/impl/LessonServiceimpl.java
@@ -1,13 +1,32 @@
 package com.school.project.service.impl;
 
 import com.school.project.dto.LessonDto;
+import com.school.project.exception.LessonNotFoundException;
 import com.school.project.model.entities.Lesson;
+import com.school.project.repository.LessonRepository;
 import com.school.project.service.LessonService;
 
+import java.util.Optional;
+
 public class LessonServiceimpl implements LessonService {
+
+    private final LessonRepository lessonRepository;
+
+    public LessonServiceimpl(LessonRepository lessonRepository) {
+        this.lessonRepository = lessonRepository;
+    }
+
     @Override
     public Lesson createLesson(Lesson lesson) {
-        return null;
+        lessonRepository.save(lesson);
+        return lesson;
+    }
+
+    @Override
+    public Lesson getLesson(Long id) {
+        Optional<Lesson> byId = lessonRepository.findById(id);
+        if (!byId.isPresent()) throw new LessonNotFoundException("Lesson with id : " + id + "is not found");
+        return byId.get();
     }
 
     @Override
