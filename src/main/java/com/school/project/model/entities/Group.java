5ef05a5b51446aa0ff49563115c6ed6e69e28da4
Your Name <you@example.com>
2019-06-28 09:29:57 +0200

    add startDate for Group

--- src/main/java/com/school/project/model/entities/Group.java
+++ src/main/java/com/school/project/model/entities/Group.java
@@ -1,11 +1,13 @@
 package com.school.project.model.entities;
 
-import com.school.project.AbstractEntity.AbstractEntity;
+import com.school.project.AbstractEntity.AbstractBaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -13,11 +15,16 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Group extends AbstractEntity {
+public class Group extends AbstractBaseEntity {
 
     @OneToOne
     @JoinColumn(name = "MODULE_ID", nullable = false)
     private Module module;
+
+    @CreationTimestamp
+    @Column(name = "START_DATE", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startDate;
 
     @OneToMany(cascade = CascadeType.ALL, targetEntity = User.class)
     @JoinTable(name = "GROUP_USER", joinColumns = {@JoinColumn(name = "GROUP_ID")},
