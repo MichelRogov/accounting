ca5418a5118fb490b07c54d12c895ad8a98eac1d
maximorlenco <maxim.orlenco@gmail.com>
2019-07-30 00:37:48 +0200

    Move Swagger config in AppConfig, hide ErrorBasicController from Swagger

--- src/main/java/com/school/project/configuration/AppConfiguration.java
+++ src/main/java/com/school/project/configuration/AppConfiguration.java
@@ -1,8 +1,17 @@
 package com.school.project.configuration;
 
+import com.google.common.base.Predicates;
+import org.hibernate.event.spi.PostLoadEvent;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class AppConfiguration {
@@ -11,5 +20,17 @@ public class AppConfiguration {
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return ApiInfo.DEFAULT;
     }
 }
